4f9a0faf35e788d0de5131a2aa417e72bbd0b9f0
João Vitor <joaovitorbernardo304@gmail.com>
2026-03-05 15:35:01 -0300

    fix padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import personal from "../assets/images/Personal.png";
 
 function Hero() {
   return (
-    <div className="flex flex-col items-center">
-      <div id="textos" className="w-full">
+    <div className=" flex flex-col items-center">
+      <div id="textos" className="w-full mt-0 pt-0">
         <div
           id="texto1"
-          className="font-jockey pt-6 flex-col items-center text-center leading-none w-full"
+          className="font-jockey flex-col items-center text-center leading-none w-full"
         >
           <h1 className="text-[40px] font-semibold tracking-wide text-[#090846] uppercase">
             TREINE COM
@@ -27,7 +27,7 @@ function Hero() {
           </h1>
         </div>
       </div>
-      <div id="imagem" className="mt-4 w-full flex justify-center">
+      <div id="imagem" className="w-full flex justify-center">
         <img src={personal} className="w-full h-auto object-cover" />
       </div>
     </div>
